97bbc447db124638fec5c147f244f788b49215ee
Stone Gray <stone@stone.local>
2026-01-10 23:52:51 -0500

    cleanup

--- scripts/demo-template.ts
+++
@@ -1,7 +0,0 @@
-import { renderTemplateFromFileSync } from '../src/backends/traefik/templateParser';
-import { XMagicProxyData } from '../src/types/xmagic';
-
-const data: XMagicProxyData = { target: 'http://10.0.0.1', hostname: 'example.com' };
-
-const out = renderTemplateFromFileSync('./config/template-example.yml', 'mysite', data);
-console.log(out);
